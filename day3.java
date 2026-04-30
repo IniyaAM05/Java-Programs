e03c50c6881e5103b74d7b076bc5324b154fca27
IniyaAM05 <iniyaanand97@gmail.com>
2026-04-30 15:10:12 +0530

    Java Programs_day2 and 3
    Signed-off-by: IniyaAM05 <iniyaanand97@gmail.com>

---
+++ day3.java
@@ -0,0 +1,11 @@
+class day3 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num > 5) {
+            System.out.println("Number is greater than 5");
+        } else {
+            System.out.println("Number is 5 or less");
+        }
+    }
+}
---
+++ loop3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class loop3{
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        int n;
+        do { 
+            n=s.nextInt();
+            System.out.println("User Entered: "+n);
+
+            } while (n!=0);
+    }
+    }
